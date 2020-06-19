314f7516234f5211bb60cbec69c0f6197fa64be8
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-18 21:49:27 -0500

    Diferencia de let y var

---
+++ Let y Var/letyvar.js
@@ -0,0 +1,23 @@
+'use strict'
+//Prueba con VAR 
+var numero = 40;
+console.log(numero);
+
+if (true) {
+    numero = 50;
+    console.log(numero);
+}
+
+console.log(numero);
+
+//Prueba con LET 
+
+var texto = 'Josue es el mejor programador del mundo';
+console.log(texto);
+
+if (true) {
+    let texto = 'Ahora la variable de texto está dentro del bloque con let';
+    console.log(texto);
+    
+}
+console.log(texto);
