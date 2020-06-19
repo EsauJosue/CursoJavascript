c5f5f33712ba4c597ee4c3f481bf17dd21314d3e
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-18 21:55:18 -0500

    Declaracion de constantes

---
+++ constantes.js
@@ -0,0 +1,5 @@
+'use strict'
+var web = 'jmcsoluciones.com';
+const ip = '192.168.60.1'; //Aqui es donde se define la constante y no puede ser modificada por nada.
+
+console.log('Manejo de constantes'+ ip);
