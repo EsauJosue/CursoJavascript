a9495078698ed12c29e9ff2591e9555a38981c8d
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-18 21:35:29 -0500

    Declaracion de variables con var y let

---
+++ Variables/variables.js
@@ -0,0 +1,10 @@
+'use strict'
+//Manejo de Variables
+
+var pais = "México";
+var continente = "America";
+var antiguedad = 2020;
+var concatenacion = pais + ' '+ continente+ ' '+ antiguedad;
+let numero_a = 5; 
+console.log(pais, continente, antiguedad);
+document.write(concatenacion);
