86b93eabdd48ca7f2d13c8e646b2145fbb913d55
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 18:34:22 -0500

    Ejercicio con Switch

---
+++ Switch/switch.js
@@ -0,0 +1,20 @@
+'use strict'
+var edad= 40;
+
+switch (edad) {
+    case 15:
+        console.log('Eres un chaval');
+        break;
+    case 18:
+        console.log('Empiezas a ser mayor de edad');
+        break;
+    case 30:
+        console.log('Bienvenido al tercer piso');
+        break;
+    case 40: 
+        console.log('Bienvenido al cuarto piso');
+        break;
+
+    default:
+        break;
+}
