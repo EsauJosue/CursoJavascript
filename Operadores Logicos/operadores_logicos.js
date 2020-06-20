13a269dc51a03266ddf8732347e31d469a9afe20
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 18:21:46 -0500

    Ejercicio de OR y AND

---
+++ Operadores Logicos/operadores_logicos.js
@@ -0,0 +1,25 @@
+'use strict'
+//Manejo de operadores lógicos
+
+/*
+AND (Y): &&
+OR (O) ||
+NEGACION : !
+
+*/
+
+var year = 2020;
+var element;
+
+//Negacion
+if (year != 2020) {
+console.log('El año es incorrecto');
+
+}
+
+if (year >= 2000 && year <= 2020) {
+    console.log('Vives en la edad moedrna')
+}
+
+
+
