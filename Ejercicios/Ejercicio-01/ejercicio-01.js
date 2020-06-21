e4cec666b7654ef8221404362e233d3276fafc10
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 21:11:34 -0500

    Se agrego validacion de datos, para evitar que ingresen numeros negativos y/o letras

--- Ejercicios/Ejercicio-01/ejercicio-01.js
+++ Ejercicios/Ejercicio-01/ejercicio-01.js
@@ -1,19 +1,37 @@
 'use strict'
+solicitud();
 
-var num1 = prompt('Ingresa el primer número');
-var num2 = prompt('Ingresa el segundo número'); 
+function solicitud(){
+    var num1 = prompt('Ingresa el primer número');
+    var num2 = prompt('Ingresa el segundo número');
 
-if (num1 == num2) {
-    document.getElementById('resultado').innerHTML = '<div><p>Los números <strong>son iguales</strong></p></div>';
-     
-}
-if (num1 > num2){
-    document.getElementById('resultado').innerHTML = 'El primer número es mayor'; 
+    if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2)){
+        alert('Ingresaste datos incorrectos');
+        solicitud();
+        
+    }
+    else{
+        comparacion(num1,num2);
+    }
     
 
 }
-if(num1 < num2){
-    document.getElementById('resultado').innerHTML = 'El segundo número es mayor';
+function comparacion(numero1, numero2){
+ 
+    if (numero1 == numero2) {
+        document.getElementById('resultado').innerHTML = '<div><p>Los números <strong>son iguales</strong></p></div>';
+         
+    }
+    if (numero1 > numero2){
+        document.getElementById('resultado').innerHTML = 'El primer número es mayor'; 
+        
     
+    }
+    if(numero1 < numero2){
+        document.getElementById('resultado').innerHTML = 'El segundo número es mayor';
+        
+    
+    }
 
 }
+
