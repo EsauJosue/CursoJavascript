3f68247056068db4e51228249021f029bce3c244
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 19:54:22 -0500

    Manejo de alert, prompt y confirm

---
+++ Ventanas/ventanas.js
@@ -0,0 +1,11 @@
+'use strict'
+//alertas
+alert('Esta es una alerta');
+
+//alerta de confirmacion
+var mi_resultado = confirm('Estas seguro de continuar?');
+console.log(mi_resultado);
+
+//Ingreso de Información
+var mi_edad = prompt("Que edad tienes?");
+console.log(mi_edad);
