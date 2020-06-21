c2319205806bd18de3f589c57d07a80ee966422b
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-21 08:15:42 -0500

    Cambio sin relevancia

---
+++ Ejercicios/Ejercicio-03/ejercicio-03.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var num1 = prompt('Ingresa el primer número');
+var num2 = prompt('Ingresa el segundo número'); 
+
+for (let x = num1; x < num2; x++) {
+  document.writeln(x+ '<br>');
+}
