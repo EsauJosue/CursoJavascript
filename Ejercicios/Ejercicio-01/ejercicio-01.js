8d9c729e95fded56d7baf8c10cd01013fde6ffc1
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 21:45:21 -0500

    Un ultimo cambio sin relevancia

--- Ejercicios/Ejercicio-01/ejercicio-01.js
+++ Ejercicios/Ejercicio-01/ejercicio-01.js
@@ -24,8 +24,7 @@ function comparacion(numero1, numero2){
     }
     if (numero1 > numero2){
         document.getElementById('resultado').innerHTML = 'El primer número es mayor'; 
-        
-    
+           
     }
     if(numero1 < numero2){
         document.getElementById('resultado').innerHTML = 'El segundo número es mayor';
