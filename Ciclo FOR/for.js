58b24255edc83499fc5ad7a74c51aac68b3e7e4c
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-20 19:07:58 -0500

    Ejercicio con el FOR

---
+++ Ciclo FOR/for.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function cicloFor(){
+    let numero;
+    numero = document.getElementById("valor").value;
+   //Ejecución del ciclo for
+   for (let x = 0; x < numero; x++) {
+      console.log(x);
+       
+   }
+
+}
