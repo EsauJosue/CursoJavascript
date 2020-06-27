ecc663c6edc3c9a169f339c982c65f19f4b66e8d
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-27 08:34:55 -0500

    Manejo de funciones con uso de parametros sencillos

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,7 +1,21 @@
 'use strict'
+//Funciones
+//Una función es una agrupacion reutilizable de un conjunto de instrucciones
 
-function calculadora(){
-    console.log('Hola soy una calculadora')
+function calculadora(numero1, numero2){
+    //Cojunto de instrucciones a ejecutar
+    console.log("Suma: "+ (numero1+numero2));
+    console.log("Resta: "+ (numero1-numero2));
+    console.log("Multipliación: "+ (numero1*numero2));
+    console.log("División: "+ (numero1/numero2));
+    console.log("******************************************")
+    return 'Hola soy una calculadora';
+
+
+   
 }
-
-calculadora();
+//Invocar o llamar a una función
+for (let i = 0; i <= 10; i++) {
+    calculadora(i,8);
+    
+}
