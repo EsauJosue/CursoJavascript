c9ee216ea852500d3b25ff1708b4145eeee00d4c
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-07-05 19:44:53 -0500

    Ambito de las variables

---
+++ AmbitoVariables/ambito.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function holaMundo(texto){
+    console.log(texto);
+    console.log(numero);
+    var hola_mundo;
+}
+var numero = 12;
+var texto = "Hola mundo soy una variable global";
+holaMundo(texto);
